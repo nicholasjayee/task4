1f153319e17cdb0694c78bef8cfc29776a68baaf
nicholasjayee <markmae840@gmail.com>
2025-05-21 18:59:39 +0300

    branches updated

--- main.c
+++ main.c
@@ -3,7 +3,20 @@
 int main()
 {
 
-    // main branch 10
+    // main branch 09
+    int quantity;
+
+    printf("Enter quantity: ");
+    scanf("%d", &quantity);
+
+    if (quantity >= 10)
+    {
+        printf("You qualify for a 15%% (0.15) discount \n");
+    }
+    else
+    {
+        printf("You qualify do not qualify for a discount \n");
+    }
 
     return 0;
 }
