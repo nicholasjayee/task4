e7668071b46bb8db6db77242d32fa807b7972167
nicholasjayee <markmae840@gmail.com>
2025-05-21 18:13:36 +0300

    initial

--- main.c
+++ main.c
@@ -3,25 +3,7 @@
 int main()
 {
 
-    // main branch 06
-
-    double cost_price;
-    double selling_price;
-
-    printf("Enter cost price: ");
-    scanf("%lf", &cost_price);
-
-    printf("Enter selling price: ");
-    scanf("%lf", &selling_price);
-
-    if (selling_price > cost_price)
-    {
-        printf("A profit of %.2lf was made \n", selling_price - cost_price);
-    }
-    else
-    {
-        printf("A loss of %.2lf was made \n", cost_price - selling_price);
-    }
+    // main branch 07
 
     return 0;
 }
