ca0e1e2f7cc53bc3c85aac9730ac375b5348872e
nicholasjayee <markmae840@gmail.com>
2025-05-21 18:36:06 +0300

    initial

--- main.c
+++ main.c
@@ -4,19 +4,6 @@ int main()
 {
 
     // main branch 08
-    int units;
-
-    printf("Enter number of units: ");
-    scanf("%d", &units);
-
-    if (units < 200)
-    {
-        printf("You pay %.2lf for %d units (.5)\n", units * 0.5, units);
-    }
-    else
-    {
-        printf("You pay %.2lf for %d units (.75)\n", units * 0.75, units);
-    }
 
     return 0;
 }
