91e5c64e424274280bd7f6a8efc9d116fa12155f
nicholasjayee <markmae840@gmail.com>
2025-05-21 18:19:01 +0300

    initial

--- main.c
+++ main.c
@@ -4,6 +4,19 @@ int main()
 {
 
     // main branch 07
+    double withdraw_amount;
+
+    printf("Enter withdraw amount: ");
+    scanf("%lf", &withdraw_amount);
+
+    if (withdraw_amount < 500)
+    {
+        printf("You have a 10 off withdraw charge \n");
+    }
+    else
+    {
+        printf("you do not have a withdraw charge \n");
+    }
 
     return 0;
 }
