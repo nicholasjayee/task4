988779727e0abc7953e769dd40ef4df8b5c00b1d
nicholasjayee <markmae840@gmail.com>
2025-05-21 16:35:35 +0300

    initial commit

--- main.c
+++ main.c
@@ -3,27 +3,7 @@
 int main()
 {
 
-    // main branch 02
+    // main branch 03
 
-    int employees_years;
-    float basic_salary;
-    double final_salary;
-
-    printf("Employee's years of service: ");
-    scanf("%d", &employees_years);
-
-    printf("Employee's basic salary: ");
-    scanf("%f", &basic_salary);
-
-    if (employees_years > 5)
-    {
-        final_salary = (double)basic_salary + (double)basic_salary * .05;
-        printf("Employee has a bonus. Full amount: %.2lf\n", final_salary);
-    }
-    else
-    {
-        printf("Employee does not qualify for a bonus \n");
-    }
-
-    return 0;
+        return 0;
 }
