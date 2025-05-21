f403259f446468e3b21471ca698997b211c53a22
nicholasjayee <markmae840@gmail.com>
2025-05-21 18:44:34 +0300

    initial

--- main.c
+++ main.c
@@ -3,7 +3,20 @@
 int main()
 {
 
-    // main branch 08
+    // main branch 09
+    int duration;
+
+    printf("Enter membership duration: ");
+    scanf("%d", &duration);
+
+    if (duration >= 4)
+    {
+        printf("You qualify for a 10%% (0.1) discount \n");
+    }
+    else if (duration <= 3)
+    {
+        printf("You qualify for a 5%% (0.05) discount \n");
+    }
 
     return 0;
 }
