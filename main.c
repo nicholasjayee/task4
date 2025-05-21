f0b3152834cd56cccb0056b725422caf06addfd5
nicholasjayee <markmae840@gmail.com>
2025-05-21 17:29:21 +0300

    initial

--- main.c
+++ main.c
@@ -3,27 +3,7 @@
 int main()
 {
 
-    // main branch 05
-    int working_hours;
-    double hourly_rate;
-    double total_amount;
+    // main branch 06
 
-    printf("Enter hourly rate: ");
-    scanf("%lf", &hourly_rate);
-
-    printf("Enter working hours: ");
-    scanf("%d", &working_hours);
-
-    if (working_hours > 40)
-    {
-        // > 40 --> .5+
-        total_amount = working_hours * .5 + working_hours;
-        printf("(qualified for overtime pay) total salary:%.2lf \n ", total_amount);
-    }
-    else
-    {
-        printf("total salary:%.2lf you do not qualify. \n ", working_hours * hourly_rate);
-    }
-
-    return 0;
+        return 0;
 }
