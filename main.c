f36e71b92e077ca0a505e879b308b43d45e64244
nicholasjayee <markmae840@gmail.com>
2025-05-21 17:14:47 +0300

    initial

--- main.c
+++ main.c
@@ -3,25 +3,7 @@
 int main()
 {
 
-    // main branch 04
-    double purchase_amount;
-    double total_payable_amount;
-
-    printf("Enter purchase amount: ");
-    scanf("%lf", &purchase_amount);
-
-    if (purchase_amount < 500)
-    {
-        // 0.05
-        total_payable_amount = purchase_amount + purchase_amount * 0.05;
-    }
-    else if (purchase_amount >= 500)
-    {
-        // 0.08
-        total_payable_amount = purchase_amount + purchase_amount * 0.05;
-    }
-
-    printf("The total payable amount is %.3lf \n", total_payable_amount);
+    // main branch 05
 
     return 0;
 }
