c5591fdb6532dc4a55ec5b005ef3de8aa4d7c8b3
nicholasjayee <markmae840@gmail.com>
2025-05-21 16:48:56 +0300

    initial

--- main.c
+++ main.c
@@ -3,24 +3,7 @@
 int main()
 {
 
-    // main branch 03
-    double salary;
-    int years;
-
-    printf("Enter employee salary: ");
-    scanf("%lf", &salary);
-
-    printf("Enter working period: ");
-    scanf("%d", &years);
-
-    if (salary > 5000 || years >= 2)
-    {
-        printf("Eligible for a loan \n");
-    }
-    else
-    {
-        printf("Not eligible for a loan \n");
-    }
+    // main branch 04
 
     return 0;
 }
